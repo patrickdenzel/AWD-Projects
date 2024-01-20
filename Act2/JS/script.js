53a6c8ab88f03ba7abc4762b4ee00f04121fdfcd
patrickdenzel <155804582+patrickdenzel@users.noreply.github.com>
2024-01-20 16:37:04 +0800

    Add files via upload

---
+++ Act2/JS/script.js
@@ -0,0 +1,8 @@
+const letter = document.querySelectorAll('#_letters #_letter');
+
+
+letter.forEach((letter ) => {
+    letter.addEventListener('click', ()=> {
+        letter.classList.toggle('active');
+    });
+});
